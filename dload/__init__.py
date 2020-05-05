6bf8a77ff2edd612d9fc4ee588d4f037347106c5
Michael Cohen <miccoh1994@gmail.com>
2020-05-05 20:47:11 +0200

    Allow download of large files on memory restricted machines, like a VPS

--- dload/__init__.py
+++ dload/__init__.py
@@ -75,9 +75,11 @@ def save(url, path="", overwrite=False):
 		path = path if path.strip() else c_path+os.path.sep+fn
 		if not overwrite and os.path.isfile(path):
 			return path
-		r = requests.get(url)
-		with open(path, 'wb') as f:
-			f.write(r.content)
+		with requests.get(url, stream=True) as r:
+			r.raise_for_status()
+			with open(path, 'wb') as f:
+				for chunk in r.iter_content(chunk_size=8192):
+					f.write(chunk)
 		return path
 	except:
 		pass
